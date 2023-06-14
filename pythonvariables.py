ca1c46896ccf22c92680ed39c7c25d0e3d1ad824
pavan <rayabagipavan@gmail.com>
2023-06-14 20:29:57 +0530

    variable

---
+++ pythonvariables.py
@@ -0,0 +1,13 @@
+""" Variables are containers for storing data values  """
+""" Python has no command for declaring a variable.
+
+A variable is created the moment you first assign a value to it. """
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+z = 4       # x is of type int
+z = "Sally" # x is now of type str
+print(z)
